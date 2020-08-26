0fff558b7ead32a7b53d5d5d114c5cf93ccbd1f5
Mata <mata@gobear.com>
2020-08-26 12:10:11 +0700

    test rebase master

--- cypress/integration/interview/gobear.test.js
+++ cypress/integration/interview/gobear.test.js
@@ -9,13 +9,18 @@ const gobearPage = new GobearPage
 
 describe('Travel Insurance Test', function () {
     before(function () {
+        let t = () => {
+            this
+        }
         cy.fixture('GoBear').then(function (data) {
             console.log(data)
             this.data = data;
         })
     })
+    
 
-    it('See over 3 travel insurance cards on result page', function () {
+    it('See over 3 travel insurance cards on result page 1', () => {
+        
         gobearPage.navigate()
         gobearPage.selectSGCountry()
         gobearPage.clickShowMyResult()
